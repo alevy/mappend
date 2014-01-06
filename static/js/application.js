f6e542bc1c37a12b9741e17048973e1826fbb8c8
Amit Aryeh Levy <amit@amitlevy.com>
2014-01-05 21:51:46 -0800

    Added fullscreen post editing

--- static/js/application.js
+++ static/js/application.js
@@ -32,7 +32,7 @@
     return true;
   });
 
-  $('a.icon').hover(function() {
+  $('a.reference').hover(function() {
     var link = $(this);
     var footnote = $(link.attr('href'));
     footnote.fadeIn(100);
@@ -60,4 +60,53 @@
     });
     return true;
   });
+
+  $('a[href="#zen"]').click(function(e) {
+    e.preventDefault();
+    var body = $(this).closest('.bubble')[0];
+    if (body.requestFullscreen) {
+      body.requestFullscreen();
+    } else if (body.mozRequestFullScreen) {
+      body.mozRequestFullScreen();
+    } else if (body.webkitRequestFullscreen) {
+      body.webkitRequestFullscreen();
+    } else if (body.msRequestFullscreen) {
+      body.msRequestFullscreen();
+    }
+    return true;
+  });
+
+  $('a[href="#exit-zen"]').click(function(e) {
+    e.preventDefault();
+    var body = document;
+    if (body.exitFullscreen) {
+      body.exitFullscreen();
+    } else if (body.mozCancelFullScreen) {
+      body.mozCancelFullScreen();
+    } else if (body.webkitExitFullscreen) {
+      body.webkitExitFullscreen();
+    } else if (body.msExitFullscreen) {
+      body.msExitFullscreen();
+    }
+    return true;
+  });
+
+  var fullScreenChangeHandler = function() {
+    if (document.fullscreenElement ||
+        document.mozFullScreenElement || document.webkitFullscreenElement ||
+        document.msFullscreenElement) {
+      // now in full screen mode
+      $('a[href="#zen"]').addClass('active');
+      $('a[href="#exit-zen"]').removeClass('active');
+    } else {
+      $('a[href="#zen"]').removeClass('active');
+      $('a[href="#exit-zen"]').addClass('active');
+    }
+  }
+
+  $(document).on('fullscreenchange', fullScreenChangeHandler);
+  $(document).on('mozfullscreenchange', fullScreenChangeHandler);
+  $(document).on('webkitfullscreenchange', fullScreenChangeHandler);
+  $(document).on('msfullscreenchange', fullScreenChangeHandler);
+
 })();
