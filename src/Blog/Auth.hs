4da07100c5dfb48ad5cdb78351924785fab65504
Amit Aryeh Levy <amit@amitlevy.com>
2015-12-08 13:23:28 -0500

    Remove openid login code

--- src/Blog/Auth.hs
+++ src/Blog/Auth.hs
@@ -3,7 +3,6 @@ module Blog.Auth where
 
 import Prelude hiding (div)
 
-import Control.Monad
 import Control.Monad.IO.Class
 import Data.Aeson
 import qualified Data.ByteString.Char8 as S8
@@ -11,59 +10,14 @@ import Data.Hex
 import qualified Data.Text as T
 import Data.Text.Encoding
 import Data.Maybe
-import Network.HTTP.Conduit (withManager)
-import Network.Wai.Request (appearsSecure)
 import System.Entropy
-import System.Environment (getEnvironment)
 import Web.Frank
 import Web.Simple
 import Web.Simple.Session
 import Web.Simple.Templates
-import Web.Authenticate.OpenId
 
 import Blog.Common
-import Blog.Models.Blog (blogId, findByOpenid, blogLogin)
-
-openIdController :: (T.Text -> Controller a ()) -> Controller a ()
-openIdController loginHandler = do
-  env <- liftIO $ lookup "ENV" `fmap` getEnvironment
-  when (env == Just "development") $ do
-    get "auth/login" $ do
-      openid <- queryParam' "openid_identifier"
-      loginHandler openid
-
-  get "auth/finalize" $ do
-    prms <- (map (\(k,(Just v)) -> (decodeUtf8 k, decodeUtf8 v)))
-              <$> queryString <$> request
-    oidr <- liftIO $ withManager $ authenticateClaimed prms
-    case identifier <$> oirClaimed oidr of
-      Just openid -> do
-        loginHandler openid
-      _ -> respond forbidden
-  get "auth/login" $ do
-    claimedId <- queryParam' "openid_identifier"
-    (Just host) <- requestHeader "Host"
-    secure <- appearsSecure <$> request
-    let completePage = decodeUtf8 $
-          if secure then
-            S8.concat ["https://", host, "/auth/finalize"]
-            else S8.concat ["http://", host, "/auth/finalize"]
-    fu <- liftIO $ withManager $ getForwardUrl claimedId
-                    completePage Nothing []
-    respond $ redirectTo $ encodeUtf8 fu
-
-handleLogin :: T.Text -> Controller AdminSettings ()
-handleLogin openid = do
-  mblog <- withConnection $ \conn -> liftIO $
-    findByOpenid conn openid
-  when (isJust mblog) $ do
-      sessionInsert "blogger_id" $ S8.pack $ show $ blogId $ fromJust mblog
-
-  ret <- fromMaybe "/" `fmap` sessionLookup "return_to"
-  sessionDelete "return_to"
-  csrfToken <- liftIO $ hex <$> getEntropy 32
-  sessionInsert "csrf_token" $ csrfToken
-  respond $ redirectTo ret
+import Blog.Models.Blog (blogId, blogLogin)
 
 logout :: Controller AdminSettings ()
 logout = do
--- src/Blog/Models/Blog.hs
+++ src/Blog/Models/Blog.hs
@@ -46,10 +46,6 @@ findByUsername :: Connection -> Text -> IO (Maybe Blog)
 findByUsername conn username = fmap listToMaybe $
   dbSelect conn $ addWhere "username = ?" [username] $ modelDBSelect
 
-findByOpenid :: Connection -> Text -> IO (Maybe Blog)
-findByOpenid conn openid = fmap listToMaybe $
-  dbSelect conn $ addWhere "openid = ?" [openid] $ modelDBSelect
-
 blogLogin :: Connection -> Text -> Text -> IO (Maybe Blog)
 blogLogin conn username password = do
   listToMaybe `fmap` (dbSelect conn $
